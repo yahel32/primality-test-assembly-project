30e215e832a14ab15657173d0475c3d0aa90146b
yahel amity <72753962+yahel32@users.noreply.github.com>
2023-02-04 04:00:22 +0200

    Update prime.asm

--- prime.asm
+++ prime.asm
@@ -3,6 +3,7 @@ MODEL small
 STACK 100h
 DATASEG
 Factors dw 16 dup (0)
+digits db 5 dup (0)
 first_primes dw 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 ;25 primes under 100
 seed db 0
 a dw 0
@@ -11,9 +12,223 @@ p dw 0
 emod_r dd 0
 prime_text db 'nice! this is a prime!',10,13,'$'
 not_prime_text db 'oh.. this is not  a prime. you will get it next time',10,13,'$'
+test_num dw 0
+is_p db 0              ;1-> prime      0-> not prime
+
+
+text_main_title db 'MAIN MENU',10,10,10,13,'$' ;text with the main menu title
+text_main_start db 'Press S to start',10,10,13,'$' ;text with the start message
+text_main_guide db 'Press G to see the guide',10,10,13,'$' ;text with the guide message
+text_main_exit db 'Press E to exit',10,10,13,'$' ;text with exit message
+
+guide_title db 'GUIDE',10,10,10,13,'$' ;guide as title
+guide_text1 db 'this is a program to test if a number is prime',10,10,13,'$'
+guide_text2 db 'you can write any number from 2-65535 and we will test',10,10,13,'$'
+guide_text3 db 'if the number is prime. if so we will tell you but',10,10,13,'$'
+guide_text4 db 'in case it is not, we will give you the factors',10,10,10,10,13,'$'
+guide_exit db 'Press R to return to the main menu',10,10,10,10,13,'$'
+
+
+test_title db 'TEST',10,10,10,13,'$' ;test as a title
+test_text1 db 'write a number between 2-65535, Press enter when you finish.',10,10,13,'$'
+test_text2 db 'Press Backspace to delete digits',10,10,13,'$'
+
+
+
+prime_title db 'PRIME!',10,10,10,13,'$'
+prime_text1 db 'nice!, the number:','$'
+prime_text2 db 'is prime',10,10,13,'$'
+prime_text3 db 'you can check yourself if you want..',10,10,10,10,13,'$'
+prime_text4 db 'Press R to test again any number you want',10,10,13,'$'
+prime_text5 db 'Press M to go to the main menu',10,10,13,'$'
+prime_text6 db 'Press E to exit',10,10,13,'$'
+
+
+n_prime_title db 'COMPOSITE',10,10,10,13,'$'
+n_prime_text1 db 'oh.. the number:','$'
+n_prime_text2 db 'is composite(not prime)',10,10,13,'$'
+n_prime_text3 db 'these are the factors:',10,13,'$'
+
+n_prime_text4 db 10,10,13,'$'
+n_prime_text5 db 'Press R to test again any number you want',10,13,'$'
+n_prime_text6 db 'Press M to go to the main menu',10,13,'$'
+n_prime_text7 db 'Press E to exit',10,13,'$'
+
+
+mid db 10,10,10,10,10,10,'$'
+digit1 db '-'
+digit2 db '-'
+digit3 db '-'
+digit4 db '-'
+digit5 db '-'
+midl db '                 ','$'
+key db 0
+
+
 CODESEG
-proc user_input
+
+
+proc tav_input ;wait for charecter
+	push ax
+	mov ah,1
+	int 21h
+	mov [key],al
+	pop ax
+	ret
+endp tav_input
+
+
+proc user_input ;wait for key press
+	push ax
+	mov ah,0h
+	int 16h
+	mov [key],al
+	pop ax
+	ret
 endp user_input
+
+proc start_text_mode ;start text mode
+	push ax
+	xor ah,ah
+	mov al,2
+	int 10h ;text mode
+	pop ax
+	ret
+endp start_text_mode
+
+proc print_text   ;print text
+;assume offset text is in the stack
+	push bp
+	mov bp,sp
+	push ax
+	push dx
+	mov dx,[bp+4]
+	mov ah,9h
+	int 21h ;print
+	pop dx
+	pop ax
+	pop bp
+	ret 2
+endp print_text
+
+proc main_screen
+call start_text_mode
+	
+	push offset mid
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset text_main_title
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset text_main_start
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset text_main_guide
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset text_main_exit
+	call print_text
+	ret
+
+endp main_screen
+
+
+proc guide_screen
+	call start_text_mode
+	
+	push offset mid
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset guide_title
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset guide_text1
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset guide_text2
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset guide_text3
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset guide_text4
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset guide_exit
+	call print_text
+	
+	ret
+endp guide_screen
+
+
+
+
+proc test_screen
+	
+	call start_text_mode
+	
+	push offset mid
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset test_title
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset test_text1
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset test_text2
+	call print_text
+	
+	push offset midl
+	call print_text
+	
+	mov ah,2
+	mov dl,[digit1]
+	int 21h
+	
+	mov dl,[digit2]
+	int 21h
+	
+	mov dl,[digit3]
+	int 21h
+	
+	mov dl,[digit4]
+	int 21h
+	
+	mov dl,[digit5]
+	int 21h
+	
+	
+	ret
+endp test_screen
+
+
 
 
 proc prime_test ;use Fermat's Litlle Therorem to decide if a given number in range(2-2**16-1) is prime
@@ -72,17 +287,13 @@ proc prime_test ;use Fermat's Litlle Therorem to decide if a given number in ran
 	
 	
 	PRIME:
-	mov dx,offset prime_text
-	mov ah,9h
-	int 21h
+	mov [is_p],1
 	jmp prime1
 	
 	
 	
 	NOT_PRIME:
-	mov dx,offset not_prime_text
-	mov ah,9h
-	int 21h
+	mov [is_p],0
 	
 	prime1:
 	pop dx
@@ -112,11 +323,11 @@ proc emod ;compute a**b mod p
 	mov [p],ax ;a=a b=b p=p
 	
 	cmp [b],0
-	jne cont
+	jne cont_
 	mov [word ptr emod_r],1
 	mov [word ptr emod_r+2],0
 	jmp fin
-	cont:
+	cont_:
 	
 	
 	mov ax,[b]
@@ -313,16 +524,355 @@ proc divisors_check ;find the divisors of a given number and store them in a giv
 	ret 4
 endp divisors_check
 
-proc check_pointer
-endp check_pointer
-
-proc show_guide
-endp show_guide
 
 proc prime_screen
+	call start_text_mode
+	
+	push offset mid
+	call print_text
+	
+	
+	push offset midl
+	call print_text
+	push offset prime_title
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset prime_text1
+	call print_text
+	
+	
+	
+	mov ah,2
+	mov dl,[digit1]
+	int 21h
+	
+	mov dl,[digit2]
+	int 21h
+	
+	mov dl,[digit3]
+	int 21h
+	
+	mov dl,[digit4]
+	int 21h
+	
+	mov dl,[digit5]
+	int 21h
+
+	
+	mov dl,' '
+	int 21h
+
+	push offset prime_text2
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset prime_text3
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset prime_text4
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset prime_text5
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset prime_text6
+	call print_text
+	
+	ret
 endp prime_screen
 
 proc not_prime_screen
+
+	
+	push offset Factors
+	push [test_num]
+	call divisors_check;Factors=factors of the number we already test
+	
+	call start_text_mode
+	
+	push offset mid
+	call print_text
+	
+	
+	push offset midl
+	call print_text
+	push offset n_prime_title
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset n_prime_text1
+	call print_text
+	
+	
+	mov ah,2
+	mov dl,[digit1]
+	int 21h
+	
+	mov dl,[digit2]
+	int 21h
+	
+	mov dl,[digit3]
+	int 21h
+	
+	mov dl,[digit4]
+	int 21h
+	
+	mov dl,[digit5]
+	int 21h
+
+	
+	mov dl,' '
+	int 21h
+	
+
+	push offset n_prime_text2
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset n_prime_text3
+	call print_text
+	
+	push offset midl
+	call print_text
+	
+	
+	mov cl,0
+	mov si,offset Factors
+	L_Factor:
+	cmp [si],0
+	je done_print_Factor_mid2
+	
+	
+	push ax
+	push bx
+	push cx
+	push dx
+	
+	L0:
+	xor dx,dx
+	mov ax,[si]
+	mov bx,10
+	
+	
+	cmp ax,10
+	jge cont
+	xor dx,dx
+	div bx   ; ax= ax/10       dx= ax mod 10
+	mov [digits+4],dl
+	jmp done_find_digit
+	cont:
+		xor dx,dx
+	div bx   ; ax= ax/10       dx= ax mod 10
+	mov [digits+4],dl
+	
+	cmp ax,10
+	jge cont1
+		xor dx,dx
+	div bx   ; ax= ax/100       dx= ax mod 100
+	mov [digits+3],dl
+	jmp done_find_digit
+	cont1:
+		xor dx,dx
+	div bx   ; ax= ax/100       dx= ax mod 100
+	mov [digits+3],dl
+	
+	cmp ax,10
+	jge cont2
+		xor dx,dx
+	div bx   ; ax= ax/1000       dx= ax mod 1000
+	mov [digits+2],dl
+	jmp done_find_digit
+	cont2:
+		xor dx,dx
+	div bx   ; ax= ax/1000       dx= ax mod 1000
+	mov [digits+2],dl
+	
+	
+	
+	cmp ax,10
+	jge cont3
+		xor dx,dx
+	div bx   ; ax= ax/10000       dx= ax mod 10000
+	mov [digits+1],dl
+	jmp done_find_digit
+	
+	done_print_Factor_mid2:
+	jmp done_print_Factor_mid
+	
+	
+	cont3:
+		xor dx,dx
+	div bx   ; ax= ax/10000       dx= ax mod 10000
+	mov [digits+1],dl
+	
+	cmp ax,10
+	jge cont4
+		xor dx,dx
+	div bx   ; ax= ax/100000       dx= ax mod 100000
+	mov [digits],dl
+	jmp done_find_digit
+	cont4:
+		xor dx,dx
+	div bx   ; ax= ax/100000       dx= ax mod 100000
+	mov [digits],dl
+	
+	done_find_digit:
+	
+	
+	cmp [digits],0
+	je dig2
+	mov dh,[digits]
+	add dh,30h
+	mov dl,dh
+	mov ah,2
+	int 21h
+	jmp reg_check0
+	
+	done_print_Factor_mid:
+	jmp done_print_Factor
+	
+	
+	dig2:
+	cmp [digits+1],0
+	je dig3
+	
+	mov dh,[digits+1]
+	add dh,30h
+	mov dl,dh
+	mov ah,2
+	int 21h
+	jmp reg_check1
+	
+	dig3:
+	cmp [digits+2],0
+	je dig4
+	
+	mov dh,[digits+2]
+	add dh,30h
+	mov dl,dh
+	mov ah,2
+	int 21h
+	jmp reg_check2
+	
+	L_Factor_mid:
+	jmp L_Factor
+	
+	dig4:
+	cmp [digits+3],0
+	je dig5
+	
+	mov dh,[digits+3]
+	add dh,30h
+	mov dl,dh
+	mov ah,2
+	int 21h
+	
+	dig5:
+	mov dh,[digits+4]
+	add dh,30h
+	mov dl,dh
+	mov ah,2
+	int 21h
+	
+	jmp fin_dig
+	
+	L_Factor_mid2:
+	jmp L_Factor_mid
+	
+	reg_check0:
+	
+	mov dh,[digits]
+	add dh,30h
+	mov dl,dh
+	mov ah,2
+	int 21h
+	
+	reg_check1:
+	
+	mov dh,[digits+1]
+	add dh,30h
+	mov dl,dh
+	mov ah,2
+	int 21h
+	
+	reg_check2:
+	
+	mov dh,[digits+2]
+	add dh,30h
+	mov dl,dh
+	mov ah,2
+	int 21h
+	
+	
+	mov dh,[digits+3]
+	add dh,30h
+	mov dl,dh
+	mov ah,2
+	int 21h
+	
+	
+	mov dh,[digits+4]
+	add dh,30h
+	mov dl,dh
+	mov ah,2
+	int 21h
+	
+	fin_dig:
+	
+	mov dl,','
+	mov ah,2
+	int 21h
+	
+	mov dl,' '
+	mov ah,2
+	int 21h
+	
+	
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	
+	mov [si],0
+	add si,2
+	inc cl
+	cmp cl,16 
+	jne L_Factor_mid2
+	
+	done_print_Factor:
+	
+	push offset n_prime_text4
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset n_prime_text5
+	call print_text
+	
+	
+	push offset midl
+	call print_text
+	push offset n_prime_text6
+	call print_text
+	
+	push offset midl
+	call print_text
+	push offset n_prime_text7
+	call print_text
+	
+	
+	ret
 endp not_prime_screen
 
 
@@ -360,13 +910,347 @@ start:
 	mov ds,ax
 	
 	
-	push 23
+	
+	
+	
+	;main loop
+	start_main_loop:
+	
+	call main_screen
+	
+	main_loop:
+	
+	
+	call user_input
+	
+	cmp [key],'e'
+	je exit_mid
+	cmp [key],'E'
+	je exit_mid
+	
+	cmp [key],'g'
+	je guide_loop_start
+	cmp [key],'G'
+	je guide_loop_start
+	
+	cmp [key],'s'
+	je test_loop_start
+	cmp [key],'S'
+	je test_loop_start
+	
+	jmp main_loop
+	start_main_loop_mid1:
+	jmp start_main_loop
+	
+;guide loop
+	guide_loop_start:
+	call guide_screen
+	
+	guide_loop:
+	
+	call user_input
+	
+	cmp [key],'r'
+	je start_main_loop
+	cmp [key],'R'
+	je start_main_loop
+	jmp guide_loop
+	
+	exit_mid:
+	jmp exit
+;test loop
+	test_loop_start:
+	call test_screen
+	
+	
+	test_loop:
+	call tav_input
+	
+	xor ax,ax
+	mov al,[key]
+	push ax
+	
+	call is_digit
+	
+	
+	cmp ax,1
+	jne check_delete_enter
+	
+	mov al,[key]
+	mov cl,0
+	digitL:
+	
+	cmp cl,0
+	jne n1
+	cmp [digit1],'-'
+	jne search
+	mov [digit1],al
+	jmp rewrite
+	
+	
+	start_main_loop_mid2:
+	jmp start_main_loop_mid1
+	
+	n1:
+	
+	cmp cl,1
+	jne n2
+	cmp [digit2],'-'
+	jne search
+	mov [digit2],al
+	jmp rewrite
+	
+	test_loop_start_mid:
+	jmp test_loop_start
+	
+	n2:
+	
+	cmp cl,2
+	jne n3
+	cmp [digit3],'-'
+	jne search
+	mov [digit3],al
+	jmp rewrite
+	
+	
+	n3:
+	
+	cmp cl,3
+	jne n4
+	cmp [digit4],'-'
+	jne search
+	mov [digit4],al
+	jmp rewrite
+	
+	
+	n4:
+	
+	cmp [digit5],'-'
+	jne search
+	mov [digit5],al
+	jmp rewrite
+	
+	
+	
+	
+	
+	jne search
+	
+	jmp rewrite
+	
+	search:
+	inc cl
+	cmp cl,5
+	jne digitL
+	
+	rewrite:
+	
+	jmp test_loop_start
+	
+	check_delete_enter:
+	cmp [key],8h
+	jne check_enter
+	
+	mov cl,5
+	
+	digitL5:
+	
+	cmp cl,5
+	jne d4
+	
+	cmp [digit5],'-'
+	je search1
+	mov [digit5],'-'
+	jmp test_loop_start
+	
+	test_loop_start_mid2:
+	jmp test_loop_start_mid
+	d4:
+	
+	cmp cl,4
+	jne d3
+	
+	cmp [digit4],'-'
+	je search1
+	mov [digit4],'-'
+	jmp test_loop_start
+	
+	
+	
+	d3:
+	
+	cmp cl,3
+	jne d2
+	
+	cmp [digit3],'-'
+	je search1
+	mov [digit3],'-'
+	jmp test_loop_start
+	
+	
+	
+	d2:
+	
+	cmp cl,2
+	jne d1
+	
+	cmp [digit2],'-'
+	je search1
+	mov [digit2],'-'
+	jmp test_loop_start
+	
+	
+	
+	d1:
+	cmp [digit1],'-'
+	je search1
+	mov [digit1],'-'
+	jmp test_loop_start
+	
+	
+	search1:
+	dec cl
+	cmp cl,0
+	jne digitL5
+	
+	jmp test_loop_start
+	
+	
+	
+	check_enter:
+	
+	cmp [key],13
+	jne test_loop_start_mid2
+	
+	mov al,[digit5]
+	sub al,30h
+	add [test_num],ax ;num+=digit5
+	
+	mov al,[digit4]
+	sub al,30h
+	mov bl,10
+	mul bl ;ax=10*digit4
+	add [test_num],ax ;num+=10*digit4
+	
+	mov al,[digit3]
+	sub al,30h
+	mov bl,100
+	mul bl ;ax=100*digit3
+	add [test_num],ax ;num+=100*digit3
+	
+	jmp solve
+	
+	start_main_loop_mid3:
+	jmp start_main_loop_mid2
+	
+	solve:
+	mov al,[digit2]
+	sub al,30h
+	mov bl,255
+	mul bl ;ax=1000*digit2
+	add [test_num],ax ;num+=1000*digit2
+	
+	mov al,[digit2]
+	sub al,30h
+	mov bl,255
+	mul bl ;ax=1000*digit2
+	add [test_num],ax ;num+=1000*digit2
+	
+	mov al,[digit2]
+	sub al,30h
+	mov bl,255
+	mul bl ;ax=1000*digit2
+	add [test_num],ax ;num+=1000*digit2
+	
+	mov al,[digit2]
+	sub al,30h
+	mov bl,255
+	mul bl ;ax=1000*digit2
+	add [test_num],ax ;num+=1000*digit2
+	
+	
+	mov cl,0
+	L100:
+	inc cl
+	mov al,[digit1]
+	sub al,30h
+	mov bl,100
+	mul bl ;ax=10000*digit1
+	add [test_num],ax ;num+=10000*digit1
+	
+	cmp cl,100
+	jne L100
+	
+	
+	;now test_num equal the number we are testing so
+	push [test_num]
 	call prime_test
 	
+	
+	cmp [is_p],1
+	je num_is_prime
+	
+	num_is_not_prime:
+	
+	call not_prime_screen
+	
+	
+	np_screen:
+	call user_input
+	
+	
+	
+	
+	
+	
+	
+	num_is_prime:
+	call prime_screen
+	
+	p_screen:
+	call user_input
+	
+	cmp [key],'r'
+	je test_again
+	cmp [key],'R'
+	je test_again
+	
+	
+	cmp [key],'m'
+	je main_again
+	cmp [key],'M'
+	je main_again
+	
+	
+	cmp [key],'e'
+	je exit
+	cmp [key],'E'
+	je exit
+	
+	
+	jmp p_screen
+	
+	main_again:
+	mov [digit1],'-'
+	mov [digit2],'-'
+	mov [digit3],'-'
+	mov [digit4],'-'
+	mov [digit5],'-'
+	
+	jmp start_main_loop_mid3
+	
+	
+	test_again:
+	mov [digit1],'-'
+	mov [digit2],'-'
+	mov [digit3],'-'
+	mov [digit4],'-'
+	mov [digit5],'-'
+	
+	jmp test_loop_start_mid2
 
 
 exit:
 	mov ax, 4c00h
 	int 21h
 END start
-
